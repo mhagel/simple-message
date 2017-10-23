f5aaac2378459702c775c481d8f90b63518bd383
Mark Hagelberg <mark.hagelberg@gmail.com>
2017-10-23 13:17:18 -0600

    - MessageForm - set up routing

---
+++ src/components/MessageForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import '../App.css';
+import './MessageForm.css';
+
+class MessageForm extends Component {
+  render() {
+    return (
+      <div className="MessageForm">
+        <form onSubmit={this.handleSubmit}>
+          <textarea className="inputBox" onChange={this.handleChange}></textarea>
+          <button className="btn" type="submit">Send Message</button>
+        </form>
+      </div>
+    );
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.value);
+    // POST message
+    event.preventDefault();
+  }
+};
+
+export default MessageForm;
--- src/components/SimpleMessage.js
+++ src/components/SimpleMessage.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import '../App.css';
 import './SimpleMessage.css';
+import { BrowserRouter, Route } from 'react-router-dom'
 import ContactsList from './ContactsList';
 import MessageHistory from './MessageHistory';
 
 class SimpleMessage extends Component {
   render() {
     return (
-      <div>
-        <div className="SimpleMessage">Simple Message: {this.state.greeting}</div>
-        <ContactsList />
-        <MessageHistory />
-      </div>
+      <BrowserRouter>      
+        <div className="SimpleMessage">Simple Message: {this.state.greeting}
+          <Route path='/' exact component={ContactsList} />
+          <Route path='/contacts' component={ContactsList} />
+          <Route path='/messages' component={MessageHistory} />
+        </div>
+      </BrowserRouter>
     );
   }
 
