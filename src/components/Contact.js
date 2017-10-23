ac1c1541839a67139d2408be5f28f31db5c299f5
Mark Hagelberg <mark.hagelberg@gmail.com>
2017-10-23 09:57:05 -0600

    Render contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,13 +5,13 @@ import './Contact.css';
 class Contact extends Component {
   render() {
     return (
-      <div className="Contact">Contact: {this.state.name}</div>
+      <div className="Contact">Contact: {this.state.name}, Phone: {this.state.phone}</div>
     );
   }
 
   constructor(props) {
     super(props);
-    this.state = { name: 'Henrik' };
+    this.state = { name: this.props.name, phone: this.props.phone };
   }
 };
 
--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -6,15 +6,27 @@ import Contact from './Contact';
 class ContactsList extends Component {
   render() {
     return (
-      <div className="ContactsList">Contacts: {this.state.contacts.length}
-          <Contact />
+      <div className="ContactsList">
+          {this.renderContacts()}
       </div>
     );
   }
 
   constructor(props) {
     super(props);
-    this.state = { contacts: ['Henrik', 'Arthur'] };
+    this.state = { 
+        contacts: [
+            {name: 'Henrik', phone: '(303) 555-1234'},
+            {name: 'Arthur', phone: '(720) 444-4321'},
+            {name: 'Minnie', phone: '(303) 333-5678'},
+        ]
+    };
+  }
+
+  renderContacts() {
+    return this.state.contacts.map(contact => (
+        <Contact {...contact}/>
+    ));
   }
 };
 
